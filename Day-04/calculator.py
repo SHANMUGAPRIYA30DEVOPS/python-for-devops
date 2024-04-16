7f1c264d67d523a4d8dd8e4723dd761df0471d08
SHANMUGAPRIYA30DEVOPS <104688378+SHANMUGAPRIYA30DEVOPS@users.noreply.github.com>
2024-04-16 19:14:31 +0530

    Create calculator.py

---
+++ Day-04/calculator.py
@@ -0,0 +1,41 @@
+num1 = 10
+num2 = 20
+
+'''
+print('number 1 is ' + str(num1))
+print('number 2 is ', num2)
+
+addition = num1 + num2
+print(addition)
+print(num1 + num2)
+
+subtraction = num1 - num2
+print(subtraction)
+'''
+
+#####################
+'''
+def addition():
+    n = num1 + num2
+    print(n)
+
+def subtraction():
+    n = num1 - num2
+    print(2)
+
+addition()
+subtraction()
+'''
+
+#####################
+
+
+def add(num1, num2):
+    s = num1 + num2
+    return s
+
+def sub(num1, num2):
+    return num1 - num2
+
+print(add(5,7))
+print(sub(9,18))
