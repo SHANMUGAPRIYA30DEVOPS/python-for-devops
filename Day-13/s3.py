0c353c2ab4afa3d1b81d615f5236529d9dcdaa87
SHANMUGAPRIYA30DEVOPS <104688378+SHANMUGAPRIYA30DEVOPS@users.noreply.github.com>
2024-05-14 08:40:59 +0530

    Update s3.py

--- Day-13/s3.py
+++ Day-13/s3.py
@@ -1,5 +1,5 @@
 #using boto3 - install aws cli, boto3, aws configure
-
+# aws boto3 official documentation  - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/create_bucket.html
 import boto3
 
 client = boto3.client('s3')
